9124fbe7796f54ab173395bed1b1df61f77f8746
Ondra Svec <ondrasvecveslo@gmail.com>
2023-04-04 15:17:50 +0200

    Add home page background

---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+import "../styles/home.css";
+
+export default function homePage() {
+  const container = document.createElement("div");
+  container.classList.add("container");
+  return container;
+}
